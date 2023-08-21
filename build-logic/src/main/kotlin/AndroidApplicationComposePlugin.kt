50a560cdd1291f64ab07a452fb31db6caf52453a
Ayşe Adanç <adancaysee@gmail.com>
2023-08-21 07:58:47 +0200

    buildSrc --> build-logic , all gradle problems are solved

--- build-logic/src/main/kotlin/AndroidApplicationComposePlugin.kt
+++ build-logic/src/main/kotlin/AndroidApplicationComposePlugin.kt
@@ -1,10 +1,6 @@
 import com.android.build.api.dsl.ApplicationExtension
-import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 
 class AndroidApplicationComposePlugin : Plugin<Project> {
--- build-logic/src/main/kotlin/AndroidApplicationPlugin.kt
+++ build-logic/src/main/kotlin/AndroidApplicationPlugin.kt
@@ -1,7 +1,6 @@
 import com.android.build.api.dsl.ApplicationExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.JavaVersion
 import org.gradle.kotlin.dsl.configure
 
 /**
